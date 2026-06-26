19b4733cc174f659042446a1851a25b5710e97f3
vigneshwarang47 <vigneshwaran.g47@gmail.com>
2026-06-26 20:28:53 +0530

    Logging and Exception

--- src/exception.py
+++ src/exception.py
@@ -8,12 +8,12 @@ def error_message_detail(error, error_detail:sys):
     file_name ,exc_tb.tb_lineno,str(error))
     return error_message
     
-class CustomExpection(Exception):
+class CustomException(Exception):
     def __init__(self, error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
         
-        def __str__(self):
-            return self.error_message
+    def __str__(self):
+        return self.error_message
 
     
